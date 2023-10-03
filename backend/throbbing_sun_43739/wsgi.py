97215128329ef9234d416d3c269fcce852ca9835
Crowdbotics <team@crowdbotics.com>
2023-10-03 18:36:45 +0000

    Initial commit

---
+++ backend/throbbing_sun_43739/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for throbbing_sun_43739 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "throbbing_sun_43739.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "throbbing-sun-43739",
+  url: "https://throbbing-sun-43739.botics.co",
+  api: "https://throbbing-sun-43739.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
